388f159b27b21d301832cf85cd4e2592b6d5fefd
Matt Cegielka <matt@mudbrick.org>
2025-08-27 21:04:52 -0600

    Upgrade dependencies

--- crates/nwws-receiver/src/main.rs
+++ crates/nwws-receiver/src/main.rs
@@ -1,13 +1,13 @@
 extern crate dotenv;
+use chrono::{DateTime, Utc};
 use dotenv::dotenv;
 use futures::StreamExt;
-use nwws_oi::StreamEvent;
-use nwws_oi::Server;
-use nwws_oi::Config;
 use nwws_oi::Channel;
-use uuid;
-use chrono::{DateTime, Utc};
+use nwws_oi::Config;
+use nwws_oi::Server;
+use nwws_oi::StreamEvent;
 use rusqlite::{named_params, Connection};
+use uuid;
 // use rusqlite::{named_params, Connection, Result};
 
 #[tokio::main]
@@ -33,19 +33,41 @@ async fn main() {
         password,
         server: Server::Primary,
         resource: format!("uuid/{}", uuid::Uuid::new_v4()),
-        channel: Channel::Default
+        channel: Channel::Default,
     };
     let mut stream = nwws_oi::Stream::new(conf);
-    
+
     // Create an SQLite database, so that we might shove bulletins into it.
     let dbconn = Connection::open(path).unwrap();
     // We want to store the timestamp, type (ttaa) and full text of bulletins
-    dbconn.execute("CREATE TABLE IF NOT EXISTS bulletins (time_rfc3339 text, type text, bulletin text)", []).unwrap();
-    dbconn.execute("CREATE INDEX IF NOT EXISTS time_idx ON bulletins (time_rfc3339)", []).unwrap();
-    dbconn.execute("CREATE INDEX IF NOT EXISTS type_ids ON bulletins (type)", []).unwrap();
-    dbconn.execute("CREATE INDEX IF NOT EXISTS time_type ON bulletins (time_rfc3339, type)", []).unwrap();
-    let mut stmt = dbconn.prepare("INSERT INTO bulletins VALUES (:time, :type, :text)").unwrap();
-    
+    dbconn
+        .execute(
+            "CREATE TABLE IF NOT EXISTS bulletins (time_rfc3339 text, type text, bulletin text)",
+            [],
+        )
+        .unwrap();
+    dbconn
+        .execute(
+            "CREATE INDEX IF NOT EXISTS time_idx ON bulletins (time_rfc3339)",
+            [],
+        )
+        .unwrap();
+    dbconn
+        .execute(
+            "CREATE INDEX IF NOT EXISTS type_ids ON bulletins (type)",
+            [],
+        )
+        .unwrap();
+    dbconn
+        .execute(
+            "CREATE INDEX IF NOT EXISTS time_type ON bulletins (time_rfc3339, type)",
+            [],
+        )
+        .unwrap();
+    let mut stmt = dbconn
+        .prepare("INSERT INTO bulletins VALUES (:time, :type, :text)")
+        .unwrap();
+
     // Process messages when we get them.
     while let Some(event) = stream.next().await {
         match event {
@@ -57,23 +79,31 @@ async fn main() {
                 match ttaa {
                     // Tornado warning
                     "WFUS" => {
-                        println!("{}: Tornado warning issued by {}", now.to_rfc3339(), message.cccc);
+                        println!(
+                            "{}: Tornado warning issued by {}",
+                            now.to_rfc3339(),
+                            message.cccc
+                        );
                         let _res = stmt.execute(named_params!{":time": now.to_rfc3339(), ":type": ttaa, ":text": message.message });
-                    },
+                    }
                     // Severe thunderstorm warning
                     "WUUS" => {
                         // Ignore SPC updates. Unsure why they have the same WMO ttaa as severe thunderstorm warnings.
                         if message.cccc != "KWNS" {
-                            println!("{}: Severe thunderstorm warning issued by {}", now.to_rfc3339(), message.cccc);
+                            println!(
+                                "{}: Severe thunderstorm warning issued by {}",
+                                now.to_rfc3339(),
+                                message.cccc
+                            );
                             let _res = stmt.execute(named_params!{":time": now.to_rfc3339(), ":type": ttaa, ":text": message.message });
                         }
-                    },
+                    }
                     // Severe weather statement - may update/supercede WFUS or WUUS bulletins
                     "WWUS" => {
                         let _res = stmt.execute(named_params!{":time": now.to_rfc3339(), ":type": ttaa, ":text": message.message });
-                    },
+                    }
                     // Fall through
-                    _ => ()
+                    _ => (),
                 }
                 // log::info!("{}", format!("{:#?}", message));
             }
